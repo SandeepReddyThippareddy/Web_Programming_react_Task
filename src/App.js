1864059f4312e7a8cac3c957fbd2efc7b805e23a
Sandeep Reddy <sandeep.thippareddy.ext@zeiss.com>
2023-12-05 11:51:19 -0500

    Intial Commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import HelloWorld from './HelloWorld';
+import Counter from './Counter';
+import FilteredList from './FilteredList';
+
+function App() {
+  const produce = [
+    {name: "Apple", type: "Fruit"},
+    {name: "Pineapple", type: "Fruit"} ,
+    {name: "Banana", type: "Fruit"},
+    {name: "Pear", type: "Fruit"},
+    {name: "Strawberry", type: "Fruit"},
+    {name: "Orange", type: "Fruit"},
+    {name: "Lettuce", type: "Vegetable"},
+    {name: "Cucumber", type: "Vegetable"},
+    {name: "Eggplant", type: "Vegetable"},
+    {name: "Squash", type: "Vegetable"},
+    {name: "Bell pepper", type: "Vegetable"},
+    {name: "Onion", type: "Vegetable"}, 
+
+  ];
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <HelloWorld name="Your Name" />
+        <Counter />
+        <FilteredList items={produce} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
